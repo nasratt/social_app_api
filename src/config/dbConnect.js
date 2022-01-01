040a58b9790d635aa7a484015cf5359e06608809
Nasratullah Talash <ntalash28@gmail.com>
2022-01-01 10:51:37 +0530

    first commit
    code taken from nem_boilerplate

---
+++ src/config/dbConnect.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+console.log(process.env.DB_CONNECTION_URL);
+
+const connectDB = () => {
+  mongoose
+    .connect(process.env.DB_CONNECTION_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then(() => console.log('Successfully connected to DB!'))
+    .catch((err) => {
+      console.log('Connection to DB failed!');
+      console.log(`Error: ${err.message}`);
+      process.exit();
+    });
+};
+
+export default connectDB;
---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import connectDB from './config/dbConnect.js';
+
+// ****** Basic Setup *******
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+const PORT = process.env.PORT || 8000;
+
+connectDB();
+
+app.get('/ping', (req, res) => {
+  res.status(200);
+  res.end('Server is running and can respond to requests!');
+});
+
+app.all('*', (req, res) => {
+  res.status(404);
+  res.end('Page not found!');
+});
+
+app.listen(PORT, () => {
+  console.log(`****** Server listening on port ${PORT} ******`);
+});
