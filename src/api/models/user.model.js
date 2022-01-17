5721748f9142463a01d927f6db9dd26b9a817f2b
Nasratullah Talash <ntalash28@gmail.com>
2022-01-17 09:47:06 +0530

    users module modified for friend requests

--- src/api/models/user.model.js
+++ src/api/models/user.model.js
@@ -1,21 +1,29 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 const { ObjectId } = Schema.Types;
+
+const options = {
+  minimize: false,
+  timestamps: true
+};
+/* inRequests: friend requests coming to user
+ * outRequests: friend requests user has sent to others
+ */
 const userSchema = new Schema(
   {
     fname: String,
     lname: String,
     email: { type: String, unique: true },
     hash: String,
-    profile_pic: { type: String, default: '' },
+    profilePic: { type: String, default: '' },
     verified: { type: Boolean, default: false },
     friends: { type: [ObjectId], default: [] },
-    posts: { type: [ObjectId], default: [] },
-    created_at: { type: Date, default: Date.now },
-    updated_at: { type: Date, default: Date.now }
+    inRequests: { type: [ObjectId], default: [] },
+    outRequests: { type: [ObjectId], default: [] },
+    posts: { type: [ObjectId], default: [] }
   },
-  { minimize: false, collection: 'users' }
+  options
 );
 
-const User = mongoose.model('user', userSchema);
+const User = mongoose.model('User', userSchema);
 export default User;
